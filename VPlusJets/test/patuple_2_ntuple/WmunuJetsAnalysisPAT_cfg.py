576fdaf2ea426f433bbb152f0058bd47f299170e
Raffaele <raffaele.gerosa@cern.ch>
2013-10-06 23:51:37 +0200

    new version still not complete

--- VPlusJets/test/patuple_2_ntuple/WmunuJetsAnalysisPAT_cfg.py
+++ VPlusJets/test/patuple_2_ntuple/WmunuJetsAnalysisPAT_cfg.py
@@ -1,6 +1,58 @@
 import FWCore.ParameterSet.Config as cms
+from FWCore.ParameterSet.VarParsing import VarParsing
 import pprint
-isMC = True 
+
+##########################
+### Parsing Parameters ###
+##########################
+
+options = VarParsing ('python')
+
+options.register ('isMC', True, VarParsing.multiplicity.singleton, VarParsing.varType.int,
+                   "Run this on simulation")
+
+options.register ('isQCD', False, VarParsing.multiplicity.singleton, VarParsing.varType.int,
+                   "Run the QCD Isolation Selection")
+
+options.register ('isTransverseMassCut', False, VarParsing.multiplicity.singleton, VarParsing.varType.int,
+                   "Apply a mT cut on the leptonic leg (W) of the selected events")
+
+options.register ('isHEEPID', True, VarParsing.multiplicity.singleton, VarParsing.varType.int,
+                   "Use the HEEP electron ID instead of the MVA one")
+
+options.register ('globalTag', '', VarParsing.multiplicity.singleton, VarParsing.varType.string,
+                   "Global Tag to be used")
+
+options.register ('numEventsToRun', -1, VarParsing.multiplicity.singleton, VarParsing.varType.int,
+                   "Number of events to process: -1 means all the input events")
+
+options.register ('OutputFileName', 'WmunuJetAnalysisntuple.root', VarParsing.multiplicity.singleton, VarParsing.varType.string,
+                   "Name of the output file")
+
+options.register ('reportEvery', 500, VarParsing.multiplicity.singleton, VarParsing.varType.int,
+                   "Number of events after which print the report")
+
+options.register ('runMetFilters', False, VarParsing.multiplicity.singleton, VarParsing.varType.int,
+                   "Run the python/TrackingCollections_cfi to apply met filters and primary vertex collection skim")
+
+options.register ('useSmearedCollection', True, VarParsing.multiplicity.singleton, VarParsing.varType.int,
+                   "In case of MC analysis use smearedJets and smearedMet as default")
+
+options.register ('hltPath', 'HLT_IsoMu24_*, HLT_Mu40_eta2p1*', VarParsing.multiplicity.singleton, VarParsing.varType.string,
+                   "List of HLT path to be required when running on data. Should be separetad by ")
+
+options.register ('didPhotonSmearing', True, VarParsing.multiplicity.singleton, VarParsing.varType.int,
+                   "true if in PAT the photon smearing for resolution has been done")
+
+options.register ('didTauSmearing', True, VarParsing.multiplicity.singleton, VarParsing.varType.int,
+                   "true if in PAT the tau smearing for resolution has been done")
+
+
+options.parseArguments()
+
+############################
+#### Create the Process ####
+############################
 
 process = cms.Process("demo")
 
@@ -9,25 +61,20 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 process.load('Configuration.StandardSequences.Services_cff')
 process.load('Configuration.StandardSequences.MagneticField_38T_cff')
 
-
-
 ##----- this config frament brings you the generator information ----
 process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
 process.load("PhysicsTools.HepMCCandAlgos.genParticles_cfi")
 process.load("Configuration.StandardSequences.Generator_cff")
 
-
 ##----- Detector geometry : some of these needed for b-tag -------
 process.load("TrackingTools.TransientTrack.TransientTrackBuilder_cfi")
-process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Configuration.Geometry.GeometryIdeal_cff")
 process.load("Geometry.CMSCommonData.cmsIdealGeometryXML_cfi")
 process.load("Geometry.CommonDetUnit.globalTrackingGeometry_cfi")
 process.load("RecoMuon.DetLayers.muonDetLayerGeometry_cfi")
 
-
 ##----- B-tags --------------
 process.load("RecoBTag.Configuration.RecoBTag_cff")
-
 
 ##----- Global tag: conditions database ------------
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
@@ -35,207 +82,188 @@ process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 ##----- Counter module ------------
 process.load("ElectroWeakAnalysis.VPlusJets.AllPassFilter_cfi")
 
-## import skeleton process
-#from PhysicsTools.PatAlgos.patTemplate_cfg import *
 
+#############################
+### Set of the Global Tag ###
+#############################
 
-############################################
-if not isMC:
-    process.GlobalTag.globaltag = 'GR_R_53_V10::All'
+if not options.isMC :
+    if options.globalTag !='' :
+     process.GlobalTag.globaltag = cms.string(globalTag)
+    else :
+     process.GlobalTag.globaltag = 'GR_P_V39_AN3::All'        
 else:
-    process.GlobalTag.globaltag = 'START53_V7E::All'
+    if options.globalTag !='' :
+     process.GlobalTag.globaltag = cms.string(globalTag)
+    else:
+     process.GlobalTag.globaltag = 'START53_V7E::All'
 
-OutputFileName = "WmunuJetAnalysisntuple.root"
-numEventsToRun = 500
-############################################
-########################################################################################
-########################################################################################
+###############################
+### Input Module Definition ###
+###############################
 
-##---------  W-->munu Collection ------------
-process.load("ElectroWeakAnalysis.VPlusJets.WmunuCollectionsPAT_cfi")
+process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring(''))
 
-##---------  Jet Collection ----------------
-process.load("ElectroWeakAnalysis.VPlusJets.JetCollectionsPAT_cfi")
+if options.isMC:
+    process.source.fileNames = cms.untracked.vstring('file:/data2/rgerosa/tlbsm_53x_v3_mc.root')
+else :
+    process.source.fileNames = cms.untracked.vstring('file:/data2/rgerosa/tlbsm_53x_v3_data.root')
+    
+
+################################
+### Output Module Definition ###
+################################     
+
+process.maxEvents                                = cms.untracked.PSet(input = cms.untracked.int32(options.numEventsToRun))
+process.MessageLogger.destinations               = ['cout', 'cerr']
+process.MessageLogger.cerr.FwkReport.reportEvery = options.reportEvery
+process.options                                  = cms.untracked.PSet( wantSummary = cms.untracked.bool(False) )
+#process.options                                 = cms.untracked.PSet( SkipEvent = cms.untracked.vstring('ProductNotFound'))
+
+process.TFileService = cms.Service("TFileService", 
+                                    fileName      = cms.string( options.OutputFileName ),
+                                    closeFileFast = cms.untracked.bool(False))
 
 ##---------  Vertex and track Collections -----------
 process.load("ElectroWeakAnalysis.VPlusJets.TrackCollections_cfi")
+
+process.primaryVertexFilter.src = cms.InputTag("goodOfflinePrimaryVertices");
+process.primaryVertexFilter.cut = cms.string(" ");
+
+##-------- Muon events of interest --------
+
+process.HLTMu = cms.EDFilter("HLTHighLevel",
+                              TriggerResultsTag  = cms.InputTag("TriggerResults","","HLT"),
+                              HLTPaths           = cms.vstring(),
+                              eventSetupPathsKey = cms.string(''),
+                              andOr = cms.bool(True), #----- True = OR, False = AND between the HLTPaths
+                              throw = cms.bool(False) # throw exception on unknown path names
+                            )
+
+for path in options.hltPath.split(",") :
+    
+    process.HLTMu.HLTPaths.append(path.replace(' ',''))
+    
+#######################################
+### MET shift correction on the fly ###
+#######################################
+
+from ElectroWeakAnalysis.VPlusJets.patMETSysShiftCorrection_cfi import *
+
+runCorrection = cms.string("Run2012ABCD")
+vertexCollection = cms.InputTag("goodOfflinePrimaryVertices")
+
+metShiftSystematicCorrection(process,
+                             options.isMC,
+                             options.useSmearedCollection,
+                             runCorrection,
+                             vertexCollection,
+                             options.didPhotonSmearing,
+                             options.didTauSmearing)
+
+
+##---------  W-->munu Collection ------------
+#process.load("ElectroWeakAnalysis.VPlusJets.WmunuCollectionsPAT_cfi")
+
+##---------  Jet Collection ----------------
+#process.load("ElectroWeakAnalysis.VPlusJets.JetCollectionsPAT_cfi")
+
 #
-
-
-process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(numEventsToRun)
-)
-
-process.MessageLogger.destinations = ['cout', 'cerr']
-process.MessageLogger.cerr.FwkReport.reportEvery = 1
-process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(False) )
-
-#process.options = cms.untracked.PSet( SkipEvent = cms.untracked.vstring('ProductNotFound'))
-process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring(
-     #  '/store/user/lnujj/PatTuples_8TeV_53X-v1/jdamgov/SingleMu/SQWaT_PAT_53X_2012B-13Jul2012-v1_part1v3/3e4086321697e2c39c90dad08848274b/pat_53x_test_v03_data_9_0_BNS.root'
-#       '/store/user/lnujj/PatTuples_8TeV_53X-v1/jdamgov/WJetsToLNu_TuneZ2Star_8TeV-madgraph-tarball/SQWaT_PAT_53X_Summer12_v1/829f288d768dd564418efaaf3a8ab9aa/pat_53x_test_v03_995_1_wBa.root'
-
- # 'file:/uscms_data/d3/bmahakud/ttbarB2G/CMSSW_5_3_8_patch1/src/TopQuarkAnalysis/TopPairBSM/test/tlbsm_53x_v3_mc.root'
-   'file:/uscms_data/d3/bmahakud/ZijunB2G/CMSSW_5_3_8_patch1/src/TopQuarkAnalysis/TopPairBSM/test/tlbsm_53x_v3_mc.root'
-#'/store/user/bparida/QCD_Pt-15to3000_TuneZ2star_Flat_8TeV_pythia6/QCD_Pt-15to3000_TuneZ2star_Flat_8TeV_pythia6_test/d1dd4fe4091510a6bd0f3de124216781/tlbsm_53x_v3_mc_133_1_IND.root'
-
-  #'/store/user/zixu/DYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball/DYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball_v0/d1dd4fe4091510a6bd0f3de124216781/tlbsm_53x_v3_mc_95_1_tcP.root'
-
-
-) )
-
-
-
-
-##-------- Electron events of interest --------
-process.HLTMu =cms.EDFilter("HLTHighLevel",
-     TriggerResultsTag = cms.InputTag("TriggerResults","","HLT"),
-     HLTPaths = cms.vstring('HLT_IsoMu24_*','HLT_IsoMu30_*'),
-     eventSetupPathsKey = cms.string(''),
-     andOr = cms.bool(True), #----- True = OR, False = AND between the HLTPaths
-     throw = cms.bool(False) # throw exception on unknown path names
-)
-
-process.primaryVertex.src = cms.InputTag("goodOfflinePrimaryVertices");
-process.primaryVertex.cut = cms.string(" ");
-
 ##########################################
 ## Filter to require at least two jets in the event
-process.RequireTwoJetsORboostedV = cms.EDFilter("JetsORboostedV",
-    minNumber = cms.untracked.int32(2),
-    maxNumber = cms.untracked.int32(100),
-    srcJets = cms.InputTag("ak5PFJetsLooseIdAll"),
-    srcVectorBoson = cms.InputTag("bestWmunu"),
-    minVpt = cms.untracked.double(100.),
-    minNumberPhotons = cms.untracked.int32(0)
-)
-process.RequireTwoJetsORboostedVStep = process.AllPassFilter.clone()
+#process.RequireTwoJetsORboostedV = cms.EDFilter("JetsORboostedV",
+#    minNumber = cms.untracked.int32(2),
+#    maxNumber = cms.untracked.int32(100),
+#    srcJets = cms.InputTag("ak5PFJetsLooseIdAll"),
+#    srcVectorBoson = cms.InputTag("bestWmunu"),
+#    minVpt = cms.untracked.double(100.),
+#    minNumberPhotons = cms.untracked.int32(0)
+#)
+#process.RequireTwoJetsORboostedVStep = process.AllPassFilter.clone()
 
 
 ##-------- Save V+jets trees --------
-process.VplusJets = cms.EDAnalyzer("VplusJetsAnalysis", 
-    jetType = cms.string("PF"),
+#process.VplusJets = cms.EDAnalyzer("VplusJetsAnalysis", 
+#    jetType = cms.string("PF"),
   #  srcPFCor = cms.InputTag("selectedPatJetsPFlow"),
-    srcPFCor = cms.InputTag("ak5PFJetsLooseId"),
-    srcPhoton = cms.InputTag("photons"),
-    IsoValPhoton = cms.VInputTag(cms.InputTag('phoPFIso:chIsoForGsfEle'),
-                                 cms.InputTag('phoPFIso:phIsoForGsfEle'),
-                                 cms.InputTag('phoPFIso:nhIsoForGsfEle'),
-                                                           ),
-    srcPFCorVBFTag = cms.InputTag("ak5PFJetsLooseIdVBFTag"), 
-    srcVectorBoson = cms.InputTag("bestWmunu"),
-    VBosonType     = cms.string('W'),
-    LeptonType     = cms.string('muon'),                          
-    TreeName    = cms.string('WJet'),
-    srcPrimaryVertex = cms.InputTag("goodOfflinePrimaryVertices"),                               
-    runningOverMC = cms.bool(isMC),			
-    runningOverAOD = cms.bool(False),			
+#    srcPFCor = cms.InputTag("ak5PFJetsLooseId"),
+#    srcPhoton = cms.InputTag("photons"),
+#    IsoValPhoton = cms.VInputTag(cms.InputTag('phoPFIso:chIsoForGsfEle'),
+#                                 cms.InputTag('phoPFIso:phIsoForGsfEle'),
+#                                 cms.InputTag('phoPFIso:nhIsoForGsfEle'),
+#                                                           ),
+#    srcPFCorVBFTag = cms.InputTag("ak5PFJetsLooseIdVBFTag"), 
+#    srcVectorBoson = cms.InputTag("bestWmunu"),
+#    VBosonType     = cms.string('W'),
+#    LeptonType     = cms.string('muon'),                          
+#    TreeName    = cms.string('WJet'),
+#    srcPrimaryVertex = cms.InputTag("goodOfflinePrimaryVertices"),                               
+#    runningOverMC = cms.bool(isMC),			
+#    runningOverAOD = cms.bool(False),			
 #    srcMet = cms.InputTag("patType1CorrectedPFMet"),
-    srcMet = cms.InputTag("patMetShiftCorrected"), # type1 + shift corrections
+#    srcMet = cms.InputTag("patMetShiftCorrected"), # type1 + shift corrections
 #    srcMetMVA = cms.InputTag("pfMEtMVA"),
-    srcMetMVA = cms.InputTag("tcMet"),  
-    srcGen  = cms.InputTag("ak5GenJets"),
-    srcMuons  = cms.InputTag("selectedPatMuonsPFlow"),
-    srcBeamSpot  = cms.InputTag("offlineBeamSpot"),
-    srcCaloMet  = cms.InputTag("patMETs"),
-    srcgenMet  = cms.InputTag("genMetTrue"),
-    srcGenParticles  = cms.InputTag("genParticles"),
-    srcTcMet    = cms.InputTag("patMETsAK5TC"),
-    srcJetsforRho = cms.string("kt6PFJetsPFlow"),                               
-    srcJetsforRho_lepIso = cms.string("kt6PFJetsForIsolation"),       
-    srcJetsforRhoCHS = cms.string("kt6PFJetsChsPFlow"),
-    srcJetsforRho_lepIsoCHS = cms.string("kt6PFJetsChsForIsolationPFlow"),
-    srcFlavorByValue = cms.InputTag("ak5tagJet"),
-    bTagger=cms.string("simpleSecondaryVertexHighEffBJetTags"),
-
-    applyJECToGroomedJets=cms.bool(True),
-    doGroomedAK5 = cms.bool(True),
+#    srcMetMVA = cms.InputTag("tcMet"),  
+#    srcGen  = cms.InputTag("ak5GenJets"),
+#    srcMuons  = cms.InputTag("selectedPatMuonsPFlow"),
+#    srcBeamSpot  = cms.InputTag("offlineBeamSpot"),
+#    srcCaloMet  = cms.InputTag("patMETs"),
+#    srcgenMet  = cms.InputTag("genMetTrue"),
+#    srcGenParticles  = cms.InputTag("genParticles"),
+#    srcTcMet    = cms.InputTag("patMETsAK5TC"),
+#    srcJetsforRho = cms.string("kt6PFJetsPFlow"),                               
+#    srcJetsforRho_lepIso = cms.string("kt6PFJetsForIsolation"),       
+#    srcJetsforRhoCHS = cms.string("kt6PFJetsChsPFlow"),
+#    srcJetsforRho_lepIsoCHS = cms.string("kt6PFJetsChsForIsolationPFlow"),
+#    srcFlavorByValue = cms.InputTag("ak5tagJet"),
+#    bTagger=cms.string("simpleSecondaryVertexHighEffBJetTags"),
+#    applyJECToGroomedJets=cms.bool(True),
+#    doGroomedAK5 = cms.bool(True),
  #   doGroomedAK6 = cms.bool(True),
-    doGroomedAK7 = cms.bool(True),
-    doGroomedAK8 = cms.bool(True),
-    doGroomedAK10 = cms.bool(True),
-    doGroomedAK12 = cms.bool(True),
-    doGroomedCA8 = cms.bool(False),
-    doGroomedCA12 = cms.bool(False)
-)
+#    doGroomedAK7 = cms.bool(True),
+#    doGroomedAK8 = cms.bool(True),
+#    doGroomedAK10 = cms.bool(True),
+#    doGroomedAK12 = cms.bool(True),
+#    doGroomedCA8 = cms.bool(False),
+#    doGroomedCA12 = cms.bool(False)
+#)
 
-if isMC:
-    process.VplusJets.JEC_GlobalTag_forGroomedJet = cms.string("START53_V15")
-else:
-    process.VplusJets.JEC_GlobalTag_forGroomedJet = cms.string("FT_53_V10_AN3")
-
-
-
-process.TFileService = cms.Service(
-    "TFileService", 
-    fileName = cms.string( OutputFileName ),
-    closeFileFast = cms.untracked.bool(False)
-)
+#if isMC:
+#    process.VplusJets.JEC_GlobalTag_forGroomedJet = cms.string("START53_V15")
+#else:
+#    process.VplusJets.JEC_GlobalTag_forGroomedJet = cms.string("FT_53_V10_AN3")
 
 
-##
-## MET shift correction on the fly
-##
-#from PhysicsTools.PatAlgos.tools.metTools import addPfMET
-
-process.load("JetMETCorrections/Type1MET/pfMETsysShiftCorrections_cfi")
-process.pfMEtSysShiftCorr.src = cms.InputTag('patMETsPFlow')
-process.pfMEtSysShiftCorr.srcMEt = cms.InputTag('patMETsPFlow')
-process.pfMEtSysShiftCorr.srcJets = cms.InputTag('selectedPatJetsPFlow')
-
-if isMC:
-# pfMEtSysShiftCorrParameters_2012runAplusBvsNvtx_mc
-  process.pfMEtSysShiftCorr.parameter = cms.PSet(
-    numJetsMin = cms.int32(-1),
-    numJetsMax = cms.int32(-1),
-    px = cms.string("+2.22335e-02 - 6.59183e-02*Nvtx"),
-    py = cms.string("+1.52720e-01 - 1.28052e-01*Nvtx")
-  )
-else:
-# pfMEtSysShiftCorrParameters_2012runAplusBvsNvtx_data
-  process.pfMEtSysShiftCorr.parameter = cms.PSet(
-    numJetsMin = cms.int32(-1),
-    numJetsMax = cms.int32(-1),
-    px = cms.string("+1.68804e-01 + 3.37139e-01*Nvtx"),
-    py = cms.string("-1.72555e-01 - 1.79594e-01*Nvtx")
-  )
-
-process.patMetShiftCorrected = cms.EDProducer("CorrectedPATMETProducer",
-    src = cms.InputTag('patMETsPFlow'),
-    applyType1Corrections = cms.bool(True),
-    srcType1Corrections = cms.VInputTag(
-        cms.InputTag('pfMEtSysShiftCorr')
-    ),
-    applyType2Corrections = cms.bool(False)
-)   
-
-process.myseq = cms.Sequence(
-    process.pfMEtSysShiftCorrSequence *
-    process.patMetShiftCorrected *
-    process.TrackVtxPath *
-    process.HLTMu *
-    process.WPath *
-    process.GenJetPath *
+process.myseq = cms.Sequence( process.TrackVtxPath *
+                              process.HLTMu*
+                              process.metShiftSystematicCorrectionSequence)
+#                              process.pfMEtSysShiftCorrSequence *
+#                              process.patMetShiftCorrected )
+#    process.WPath *
+#    process.GenJetPath *
 ##    process.btagging * 
-    process.TagJetPath *
-    process.PFJetPath *
-    process.RequireTwoJetsORboostedV *
-    process.RequireTwoJetsORboostedVStep
-    )
+#    process.TagJetPath *
+#    process.PFJetPath *
+#    process.RequireTwoJetsORboostedV *
+#    process.RequireTwoJetsORboostedVStep
+#    )
 
-if isMC:
-    process.myseq.remove ( process.noscraping)
-    process.myseq.remove ( process.HBHENoiseFilter)
-    process.myseq.remove ( process.HLTMu)
-else:
-    process.myseq.remove ( process.noscraping)
-    process.myseq.remove ( process.HBHENoiseFilter)
-    process.myseq.remove ( process.GenJetPath)
-    process.myseq.remove ( process.TagJetPath)
+if options.isMC:
+     process.myseq.remove ( process.HLTMu)
+#else:
+#    process.myseq.remove ( process.GenJetPath)
+#    process.myseq.remove ( process.TagJetPath)
 
+if not options.runMetFilters :
+    process.myseq.remove(process.TrackVtxPath)
 
-##---- if do not want to require >= 2 jets then disable that filter ---
-##process.myseq.remove ( process.RequireTwoJets)  
+process.p = cms.Path( process.myseq)
+#* process.VplusJets)
 
-#process.outpath.remove(process.out)
-process.p = cms.Path( process.myseq  * process.VplusJets)
+############################
+## Dump the output Python ##
+############################
+
+processDumpFile = open('processDump.py', 'w')
+print >> processDumpFile, process.dumpPython()
+   
